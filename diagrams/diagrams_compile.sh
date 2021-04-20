2c61cf10b94e4c0bce0d1ebd09a971e84bb4b84b
Hugo Protsch <hugo.protsch@gmail.com>
2021-04-20 21:04:56 +0200

    Add umlet compile step

---
+++ diagrams/diagrams_compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp -r diagrams out
+cd out
+rm compile.sh
+for f in $(find -name '*.uxf')
+do 
+	echo $f
+	output=${f%.uxf}.pdf
+	echo $output
+	/Umlet/umlet.sh -action=convert -format=pdf -filename=$f -output=$output
+	rm $f
+done
