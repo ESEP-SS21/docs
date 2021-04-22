542c27c20b1b13eb9a6ed300486d2e8909a9cfc4
Hugo Protsch <hugo.protsch@gmail.com>
2021-04-22 20:48:30 +0200

    Fix pipeline

--- diagrams/compile.sh
+++ diagrams/compile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-cp -r diagrams out
-cd out
+cd ..
+rm -rf out/diagrams || true
+mkdir -p out
+
+cp -r diagrams out/diagrams
+cd out/diagrams
 rm compile.*
 for f in $(find -name '*.uxf')
-do 
+do
 	echo "$f"
 	output=${f%.uxf}.pdf
 	echo "$output"
 	/Umlet/umlet.sh -action=convert -format=pdf -filename="$f" -output="$output"
 	rm "$f"
 done
-mv ./* diagrams/
